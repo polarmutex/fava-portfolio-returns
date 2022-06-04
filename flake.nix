95d3f61c057d4510c945b1b65afb1ef74efa5888
Brian Ryall <brian@brianryall.xyz>
2022-06-04 16:06:52 -0400

    chore: switch to nix and poetry

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  #inputs.polar-nur.url = "github:polarmutex/nur";
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, polar-nur }:
+    {
+      # Nixpkgs overlay providing the application
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+      ];
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlay
+            #polar-nur.overlays.default
+          ];
+        };
+      in
+      {
+        devShell =
+          let
+            fava_portfolio_env = pkgs.poetry2nix.mkPoetryEnv {
+              projectDir = ./.;
+              editablePackageSources = {
+                fava_portfolio_returns = ./fava_portfolio_returns;
+              };
+              overrides = pkgs.poetry2nix.overrides.withDefaults (
+                self: super: {
+                  numpy = pkgs.python39.pkgs.numpy;
+                  python-magic = pkgs.python39.pkgs.python-magic;
+                }
+              );
+            };
+          in
+          pkgs.mkShell {
+            buildInputs = [
+              fava_portfolio_env
+              pkgs.poetry
+            ];
+          };
+      }));
+}
